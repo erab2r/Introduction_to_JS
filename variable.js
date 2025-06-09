fd135cb772db8e506b7e68358f41831c13856f31
erab2r <shkera213@gmail.com>
2025-06-09 17:22:44 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,9 @@
+var apple_price = 20;
+var orange_price = parseFloat('22.5');
+console.log(apple_price + orange_price); 
+
+var first_price = 60.556780;
+var second_price = 45.60000;
+var total = first_price + second_price;
+console.log(total.toFixed(1));  
+
