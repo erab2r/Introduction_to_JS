fd62b431077122c0f539ae330762100e7bc36334
erab2r <shkera213@gmail.com>
2025-06-11 01:03:40 +0600

    ..

---
+++ if.js
@@ -0,0 +1,7 @@
+var weight = 40;
+if(weight < 20){
+    console.log("I will carry it by myself");
+}
+else{
+   console.log("I will rent a rickshaw"); 
+}
---
+++ mltcondi.js
@@ -0,0 +1,9 @@
+const salary = 30000;
+const isBCS = true;
+const height = 65;
+if(salary > 20000 && height > 68){
+    console.log("Su---putro");
+}
+else{
+    console.log("Onno putro dekho");
+}
