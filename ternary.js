65e249a97f47d0edfdb12069b7a0be943b7ac49e
erab2r <shkera213@gmail.com>
2025-06-11 04:33:15 +0600

    .........

--- ternary.js
+++ ternary.js
@@ -1,2 +1,15 @@
 const age = 16;
 age >= 18 ? console.log('Vote dio') : console.log('Ghumai thakio');
+
+let price = 500;
+const isLeader = true;
+
+// if(isLeader === true){
+//     price = 0;
+// }
+// else{
+//     price = price + 100;
+// }
+
+// console.log(price);
+price = isLeader === true ? 0 : price + 100;
