e186bb1c5f564dec5c68ed24b9ca13e535788d22
erab2r <shkera213@gmail.com>
2025-06-11 00:07:07 +0600

    .

---
+++ comparison.js
@@ -0,0 +1,6 @@
+console.log(5>10);
+console.log(5<10);
+console.log(5==10);
+console.log(5==5);
+console.log(15>=5);
+console.log(55<=55);
---
+++ nd.js
@@ -0,0 +1,6 @@
+// You went to the supermarket to buy some oranges and apples.calculate how much money the shopkeeper will return.Input:The 1st line of the input is the taka you hav.the second line is the cost of 1kg of oranges and 1kg of apples.
+
+let taka = parseInt(("Enter tk you have: "));
+let cost = parseInt(("Total cost of apple and orange: "));
+let total_tk = taka - cost;
+console.log(total_tk);
