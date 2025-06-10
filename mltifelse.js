a225a1404ce94b15074e5afa30881280373a2858
erab2r <shkera213@gmail.com>
2025-06-11 01:43:51 +0600

    ..

--- mltifelse.js
+++ mltifelse.js
@@ -4,11 +4,13 @@ if(price >= 5000){
     const payAmount = price - discount;
     console.log(payAmount);
 }
+
 else if(price >= 2600){
     const discount = price * 5/100;
     const payAmount = price - discount;
     console.log(payAmount);
 }
+
 else{
     console.log(price);
 }
