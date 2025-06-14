a1d3fa541db4c8d787f4818977a6fa828e97a5a4
erab2r <shkera213@gmail.com>
2025-06-14 23:47:37 +0600

    ,,,

---
+++ concat.js
@@ -0,0 +1,9 @@
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+const frontend = ['HTML', 'CSS', 'JavaScript'];
+const backend = ['Node.js', 'Express.js', 'MongoDB'];
+const fullStack = frontend.concat(backend);
+console.log('Frontend :', frontend);
+console.log('Backend :', backend);
+console.log('Full Stack Technologies:', fullStack);
