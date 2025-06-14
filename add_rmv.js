67d190ea447dc7a04a17aba8c069fc8cb0bdacbb
erab2r <shkera213@gmail.com>
2025-06-14 22:06:44 +0600

    ./;'

---
+++ add_rmv.js
@@ -0,0 +1,12 @@
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+const tourist_sports = ['paris','italy','spain'];
+tourist_sports.push('Bangladesh');
+tourist_sports.push('India','Nepal');
+tourist_sports.pop();
+console.log(tourist_sports);
+
