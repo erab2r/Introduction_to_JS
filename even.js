b1084d6dbca9b0ffda2c204d3cf78b95b973710b
erab2r <shkera213@gmail.com>
2025-06-15 13:23:21 +0600

    k

---
+++ even.js
@@ -0,0 +1,11 @@
+// Find all the even numbers from 78 to 98.
+let num = 78;
+while(num <=98){
+    if(num%2 ==0){
+        console.log('Even',num);
+    }
+   else {
+        console.log('Odd', num);
+    }
+      num++;
+}
---
+++ odd.js
@@ -0,0 +1,8 @@
+// Find all the odd numbers from 61 to 100.
+let num = 61;
+while(num <=100){
+    if(num%2 !==0){
+        console.log(num);
+    }
+      num++;
+}
