1d7efe73e294700570232249961ccc388ee5d91f
erab2r <shkera213@gmail.com>
2025-06-16 22:15:36 +0600

    jjjj

--- push_pop.js
+++ push_pop.js
@@ -4,6 +4,7 @@ numbers.push(200);
 numbers.push(59);
 numbers.push(78,68,98);
 console.log(numbers);
+
 const friends = ['sayed','saif','samiul','sajib'];
 friends.push('Khadiza');
 console.log(friends);
