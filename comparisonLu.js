9125b438943a2a330980a3acc756e223dba2c557
erab2r <shkera213@gmail.com>
2025-06-23 00:53:11 +0600

    yioou

---
+++ comparisonLu.js
@@ -0,0 +1,9 @@
+const subject = 'chemistry';
+const book = 'Chemistry';
+
+if(subject === book){
+    console.log('I am reading book');
+}
+else{
+    console.log('Hudai hudai prista ultai ar vat khawar');
+}
