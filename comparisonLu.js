29fb969efe890f0ff50dfedb08460eed163a8ac2
erab2r <shkera213@gmail.com>
2025-06-24 14:01:08 +0600

    dfgsdg

--- comparisonLu.js
+++ comparisonLu.js
@@ -8,3 +8,14 @@ if(subject.toLowerCase === book.toLowerCase){
 else{
     console.log('Hudai hudai prista ultai ar vat khawar');
 }
+
+const drink = ' water';
+const liquid = '     water';
+
+if(drink.trim() === liquid.trim() ){
+    console.log('pani er opor nam jibon');
+}
+
+else{
+    console.log('somudre pani ase khaite pari na');
+}
