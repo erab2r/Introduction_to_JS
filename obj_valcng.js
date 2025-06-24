ca390e04779bfa702ea1189dc53b5da163982927
erab2r <shkera213@gmail.com>
2025-06-25 00:25:55 +0600

    bhkjhkj

---
+++ obj_valcng.js
@@ -0,0 +1,10 @@
+const person = {
+    name : 'Boddar Udsin',
+    age: 25,
+    profession: 'Development',
+    salary: 2500,
+    married:true,
+    'fav places':['Bandarban','Sainmartin','kuakata'],
+}
+person["fav places"] = ['Maldives','bali','patya'];
+console.log(person);
