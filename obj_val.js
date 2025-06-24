7662697e6a6bbd777768ed61c7738b9aa56cf691
erab2r <shkera213@gmail.com>
2025-06-25 00:42:15 +0600

    gdfsgfd

---
+++ obj_val.js
@@ -0,0 +1,9 @@
+const computer = {
+    brand : 'Lenovo',
+    price: 35000,
+    processor: 'intel',
+    hd:'512gb',
+    monitor:'hp',
+}
+const values = Object.values(computer);
+console.log(values);
