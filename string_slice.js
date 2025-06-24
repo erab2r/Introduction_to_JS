f56fe58dba79c65bbe0436fc919b1fd755e6a3fe
erab2r <shkera213@gmail.com>
2025-06-24 16:39:53 +0600

    gsdgf

---
+++ string_slice.js
@@ -0,0 +1,8 @@
+const address = 'Muradpurctg';
+const part = address.slice(2,6);
+console.log(part);
+
+const sentence = 'I am Israt Jahan Era';
+console.log(sentence.split());
+console.log(sentence.split(''));
+console.log(sentence.split(' '));
